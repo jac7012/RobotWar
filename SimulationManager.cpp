f08165c52809fc2892b0f9e46c41a2267ee08b90
LIM E JAC <limejac31@gmail.com>
2025-05-20 11:20:22 +0800

    use namespace std for SimulationManager.cpp

--- SimulationManager.cpp
+++ SimulationManager.cpp
@@ -14,49 +14,50 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+using namespace std;
 
-SimulationManager::SimulationManager(const std::string& inputFile)
+SimulationManager::SimulationManager(const string& inputFile)
     : battlefield(0, 0), steps(0), robotCount(0) {
     parseInputFile(inputFile);
 }
 
 void SimulationManager::runSimulation() {
-    std::cout << "Simulation initialized.\n";
-    std::cout << "Battlefield size: " << battlefield.getWidth() << " x " << battlefield.getHeight() << "\n";
-    std::cout << "Steps to simulate: " << steps << "\n";
-    std::cout << "Number of robots: " << robotCount << "\n";
+    cout << "Simulation initialized.\n";
+    cout << "Battlefield size: " << battlefield.getWidth() << " x " << battlefield.getHeight() << "\n";
+    cout << "Steps to simulate: " << steps << "\n";
+    cout << "Number of robots: " << robotCount << "\n";
 
     
     battlefield.display();
 }
 
-void SimulationManager::parseInputFile(const std::string& inputFile) {
-    std::ifstream file(inputFile);
+void SimulationManager::parseInputFile(const string& inputFile) {
+    ifstream file(inputFile);
     if (!file.is_open()) {
-        std::cerr << "Error: Cannot open input file: " << inputFile << "\n";
+        cerr << "Error: Cannot open input file: " << inputFile << "\n";
         return;
     }
 
-    std::string line;
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
+    tring line;
+    while (getline(file, line)) {
+        istringstream iss(line);
 
-        if (line.find("M by N") != std::string::npos) {
-            std::string dummy;
+        if (line.find("M by N") != string::npos) {
+           string dummy;
             int width, height;
             iss >> dummy >> dummy >> dummy >> width >> height;
             battlefield = Battlefield(width, height);
         }
-        else if (line.find("steps:") != std::string::npos) {
+        else if (line.find("steps:") != string::npos) {
             std::string dummy;
             iss >> dummy >> steps;
         }
-        else if (line.find("robots:") != std::string::npos) {
-            std::string dummy;
+        else if (line.find("robots:") != string::npos) {
+            string dummy;
             iss >> dummy >> robotCount;
         }
         else if (!line.empty() && isalpha(line[0])) {
-            std::string type, name, xStr, yStr;
+            string type, name, xStr, yStr;
             iss >> type >> name >> xStr >> yStr;
 
             int x, y;
@@ -64,11 +65,11 @@ void SimulationManager::parseInputFile(const std::string& inputFile) {
                 x = rand() % battlefield.getWidth();
                 y = rand() % battlefield.getHeight();
             } else {
-                x = std::stoi(xStr);
-                y = std::stoi(yStr);
+                x = stoi(xStr);
+                y = stoi(yStr);
             }
 
-            std::cout << "Parsed robot: " << type << " " << name << " at (" << x << "," << y << ")\n";
+            cout << "Parsed robot: " << type << " " << name << " at (" << x << "," << y << ")\n";
             battlefield.placeRobot(x, y);
         }
     }
