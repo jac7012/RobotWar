d77b4b65c4248848dcc4da6298009d315c15c34a
LIM E JAC <limejac31@gmail.com>
2025-05-27 16:49:31 +0800

    added the moving, seeing, shooting and thinking robot

--- newrobot.h
+++ newrobot.h
@@ -2,6 +2,8 @@
 #define NEWROBOT_H
 
 #include <string>
+#include <vector>
+#include "Battlefield.h"
 
 using namespace std;
 
@@ -20,6 +22,8 @@ protected:
     bool alive;
     int fieldWidth;
     int fieldHeight;
+    bool hidden = false; // Indicates if the robot is hidden
+    bool alive = true; // Indicates if the robot is alive
 
 public:
     Robot(string n, int x, int y, int w, int h)
@@ -61,6 +65,75 @@ public:
             }
         }
     }
+    void hide() { hidden = true; }
+    void reveal() { hidden = false; }
+    bool isHidden() const { return hidden; }
+    
+    // For upgrades
+    virtual bool canUpgrade() const = 0;
+    virtual void upgrade(const string& upgradeType) = 0;
+    };
+
+    class MovingRobot : public virtual Robot {
+protected:
+    int moveRange;
+    int jumpCount;
+    int hideCount;
+    
+public:
+    MovingRobot(const string& name, int x, int y) 
+        :  moveRange(1), jumpCount(0), hideCount(0) {}
+    
+    virtual void move(Battlefield& battlefield, int dx, int dy) = 0;
+    virtual bool canJump() const { return jumpCount > 0; }
+    virtual void jump(Battlefield& battlefield, int x, int y) = 0;
+    virtual bool canHide() const { return hideCount > 0; }
+    virtual void toggleHide() = 0;
 };
+
+class ShootingRobot : public virtual Robot {
+protected:
+    int shells;
+    int fireRange;
+    bool semiAuto;
+    
+public:
+    ShootingRobot(const string& name, int x, int y) 
+        : shells(10), fireRange(1), semiAuto(false) {}
+    
+    virtual bool canFire() const { return shells > 0; }
+    virtual void fire(Battlefield& battlefield, int dx, int dy) = 0;
+    virtual int getShells() const { return shells; }
+};
+
+class SeeingRobot : public virtual Robot {
+protected:
+    int visionRange;
+    int scoutCount;
+    int trackers;
+    vector<pair<int, int>> trackedRobots;
+    
+public:
+    SeeingRobot(const string& name, int x, int y) 
+        : visionRange(1), scoutCount(0), trackers(0) {}
+    
+    virtual vector<pair<int, int>> look(Battlefield& battlefield, int dx, int dy) = 0;
+    virtual bool canScout() const { return scoutCount > 0; }
+    virtual void scout(Battlefield& battlefield) = 0;
+    virtual bool canTrack() const { return trackers > 0; }
+    virtual void track(Battlefield& battlefield, const string& targetName) = 0;
+};
+class ThinkingRobot : public virtual Robot {
+protected:
+    vector<string> upgrades;
+    
+public:
+    ThinkingRobot(const string& name, int x, int y) ;
+    
+    virtual void analyze(Battlefield& battlefield) = 0;
+    virtual void decideAction(Battlefield& battlefield) = 0;
+    virtual vector<string> getAvailableUpgrades() const = 0;
+};
+
 
 #endif // NEWROBOT_H
