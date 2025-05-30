f679951cd70ff32d453a591efe471e05935e1c73
Kirtthi <abikirthikka.tamil.arasan@student.mmu.edu.my>
2025-05-30 20:56:05 +0800

    Update DisplayManager.h

--- DisplayManager.h
+++ DisplayManager.h
@@ -9,44 +9,101 @@
  Email: ABIKIRTHIKKA.TAMIL.ARASAN@student.mmu.edu.my
  Phone: 014-9350432
  **********|**********|**********/
+#ifndef DISPLAYMANAGER_H
+#define DISPLAYMANAGER_H
 
-
-#ifndef DISPLAY_MANAGER_H
-#define DISPLAY_MANAGER_H
-
-#include "Robot.h"
 #include "Battlefield.h"
+#include "Robot.h"
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
-#include <iomanip>
+
+using namespace std;
 
 class DisplayManager {
-public:
-    DisplayManager(int width, int height);
-    ~DisplayManager();
-    
-    void initializeDisplay();
-    void updateDisplay(const std::vector<Robot*>& robots);
-    void logAction(const std::string& action);
-    void clearScreen();
-    void printBattlefield() const;
-    void saveToFile(const std::string& filename) const;
-    
-    // Helper functions for display formatting
-    std::string formatRobotInfo(const Robot* robot) const;
-    std::string formatPosition(int x, int y) const;
-    
 private:
-    int width;
-    int height;
-    std::vector<std::vector<char>> battlefield;
-    std::vector<std::string> actionLog;
-    std::ofstream logFile;
+    ofstream logFile;
+    string logFilename;
     
-    void clearBattlefield();
-    char getRobotSymbol(const Robot* robot) const;
-    void drawBorders();
+public:
+    DisplayManager(const string& filename = "battle_log.txt") : logFilename(filename) {
+        logFile.open(logFilename);
+        if (!logFile) {
+            cerr << "Error opening log file!" << endl;
+        }
+    }
+    
+    ~DisplayManager() {
+        if (logFile.is_open()) {
+            logFile.close();
+        }
+    }
+    
+    void displayBattlefield(const Battlefield& battlefield) {
+        // Clear screen (platform specific)
+        #ifdef _WIN32
+            system("cls");
+        #else
+            system("clear");
+        #endif
+        
+        cout << "\nCurrent Battlefield:\n";
+        for (int i = 0; i < battlefield.getHeight(); ++i) {
+            for (int j = 0; j < battlefield.getWidth(); ++j) {
+                cout << battlefield.getCell(i, j) << ' ';
+            }
+            cout << endl;
+        }
+        cout << endl;
+        
+        // Log to file
+        logFile << "\nCurrent Battlefield:\n";
+        for (int i = 0; i < battlefield.getHeight(); ++i) {
+            for (int j = 0; j < battlefield.getWidth(); ++j) {
+                logFile << battlefield.getCell(i, j) << ' ';
+            }
+            logFile << endl;
+        }
+        logFile << endl;
+    }
+    
+    void displayRobotAction(const string& action) {
+        cout << action << endl;
+        logFile << action << endl;
+    }
+    
+    void displayTurnHeader(int turn) {
+        string header = "--- Turn " + to_string(turn) + " ---";
+        cout << header << endl;
+        logFile << header << endl;
+    }
+    
+    void displayGameOver(const vector<Robot*>& robots) {
+        int aliveCount = 0;
+        string winner;
+        
+        for (const auto& robot : robots) {
+            if (robot->isLives()) {
+                aliveCount++;
+                winner = robot->getName();
+            }
+        }
+        
+        if (aliveCount <= 1) {
+            string message = (aliveCount == 1) 
+                ? "Game Over! Winner: " + winner
+                : "Game Over! All robots destroyed!";
+                
+            cout << message << endl;
+            logFile << message << endl;
+        }
+    }
+    
+    void displayRobotStats(const Robot* robot) {
+        robot->displayStats();
+        // You could also log this if needed
+    }
 };
 
-#endif // DISPLAY_MANAGER_H
+#endif // DISPLAYMANAGER_H
